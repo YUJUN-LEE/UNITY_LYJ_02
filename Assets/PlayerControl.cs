acb92880e6d15dce824935943a6adc222e3620f4
YUJUN-LEE <by0903@naver.com>
2024-01-30 00:34:10 +0900

    2024_01_30_UNITY_02

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerControl : MonoBehaviour
+{
+	Vector3 m_vecMoveDirection = Vector3.zero;
+	float m_fMoveSpeed = 0.1f; // 움직이는 속도
+	float m_fJumpForce = 5f; // 점프 하는 힘
+    float TimeCount = 0f; // 시간 카운트
+    int JumpCount = 0; // 점프 횟수 체크
+    bool isGround = true; // 땅에 닿아있는지 확인
+    // Start is called before the first frame update
+    void Start()
+	{
+		
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		MoveComponent();
+        Jump();
+        TimeCount += Time.deltaTime;
+        // Debug.Log(Time.deltaTime.ToString()); // Time.deltaTime = 1 프레임 당 처리 시간
+        // transform.Translate(m_vecMoveDirection * Time.deltaTime * 3f); 추천하는 방법 X
+        // Time.timeScale(값) : 재생속도, 기본 값 1
+    }
+	private void FixedUpdate()
+	{
+		transform.Translate(m_vecMoveDirection * m_fMoveSpeed);
+        
+    }
+	void MoveComponent()
+	{
+		bool isUpKey = Input.GetKey(KeyCode.W);
+		bool isDownKey = Input.GetKey(KeyCode.S);
+		bool isRightKey = Input.GetKey(KeyCode.D);
+		bool isLeftKey = Input.GetKey(KeyCode.A);
+
+		if (isUpKey)
+		{
+			m_vecMoveDirection.z = 1f;
+		}
+		else if (isDownKey) 
+		{
+			m_vecMoveDirection.z = -1f;
+		}
+		else
+		{
+			m_vecMoveDirection.z = 0f;
+		}
+		if(isRightKey)
+		{
+			m_vecMoveDirection.x = 1f;
+		}
+		else if(isLeftKey)
+		{
+			m_vecMoveDirection.x = -1f;
+		}
+		else
+		{
+			m_vecMoveDirection.x = 0f;
+		}
+	}
+
+	void Jump()
+	{
+        bool isjump = Input.GetKeyDown(KeyCode.Space);
+
+        if (isjump && isGround) // isjump가 true이고 isGround가 true면 실행
+		{
+			m_vecMoveDirection.y = 1f; // y축 +1 방향 설정
+			transform.Translate(m_vecMoveDirection * m_fJumpForce * Time.deltaTime); // 위치 변경(방향 * 점프하는 힘 * 시간(자연스럽게 올라가게 만드는 용도))
+			TimeCount = 0;
+			isGround = false; // 점프를 했으므로 땅에서 떨어짐
+			JumpCount = JumpCount + 1; // 점프 횟수 증가
+			Debug.Log("점프 입력");
+            Debug.Log(JumpCount);
+        }
+        else if (!isGround && TimeCount <= 1 && isjump && JumpCount < 2) // isGround가 false고 TimeCount(체공 시간)가 1보다 작고 isjump가 true이며 JumpCount가 2보다 작을 때 실행
+        {
+            m_vecMoveDirection.y = 1f;
+            transform.Translate(m_vecMoveDirection * m_fJumpForce * Time.deltaTime);
+            TimeCount = 0; // 체공 시간 초기화
+            JumpCount = JumpCount + 1; // 점프 횟수 증가
+            Debug.Log("공중에서 점프");
+            Debug.Log(JumpCount);
+        }
+
+        if (!isGround && TimeCount >= 1 && !isjump) // isGround가 false이고 체공 시간이 1초를 넘으며 isjump가 false일 때 실행
+		{
+			m_vecMoveDirection.y = -1f; // y축 -1 방향 설정
+            transform.Translate(m_vecMoveDirection * m_fJumpForce * Time.deltaTime);
+			TimeCount = 0; // 체공 시간 초기화
+        }
+
+        if (!isGround && transform.position.y <= 0 && !isjump) // isGround가 false이고 y의 위치가 0보다 작거나 같고 isjump가 false일 때 실행
+        {
+            m_vecMoveDirection.y = 0f; // y축 0 방향 설정
+            transform.Translate(m_vecMoveDirection * m_fJumpForce * Time.deltaTime);
+            isGround = true; // 땅에 닿았으니 true로 변경
+            JumpCount = 0; // 점프 횟수 초기화
+            Debug.Log("바닥에 닿음");
+            Debug.Log(JumpCount);
+        }
+    }
+}
---
+++ Assets/STUDY.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class STUDY : MonoBehaviour
+{
+	// 각 축(x, y, z)의 이동 방향, 값은 3가지(-1, 0, 1)만 입력
+	float fDirectionX = 0;
+	float fDirectionY = 0;
+	float fDirectionZ = 0;
+	// 이동 속도
+	float fMoveSpeed = 0.1f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// 함수 호출
+		ProcessInput();
+	}
+	private void FixedUpdate()
+	{
+		transform.Translate(fDirectionX * fMoveSpeed, fDirectionY * fMoveSpeed, fDirectionZ * fMoveSpeed);
+	}
+	// 입력 처리 함수 정의
+	void ProcessInput()
+	{
+		// 누르고 있는 동안 모든 프레임에서 true값 반환
+		// Input.GetKey(KeyCode.UpArrow);
+		// 키보드를 누른 프레임에 true값 반환
+		// Input.GetKeyDown(KeyCode.DownArrow);
+		// 키보드를 놓은 프레임에 true값 반환
+		// Input.GetKeyUp(KeyCode.RightArrow);
+
+		bool isUpKey = Input.GetKey(KeyCode.UpArrow);
+		bool isDownKey = Input.GetKey(KeyCode.DownArrow);
+		bool isRightKey = Input.GetKey(KeyCode.RightArrow);
+		bool isLeftKey = Input.GetKey(KeyCode.LeftArrow);
+
+		if (isUpKey) // 위에 키를 눌렀을(누르고 있을) 때
+		{
+			fDirectionY = 1; // Y 방향의 값을 1로 바꿈
+		}
+		else if (isDownKey)
+		{
+			fDirectionY = -1;
+		}
+		else
+		{
+			fDirectionY = 0;
+		}
+		if (isRightKey)
+		{
+			fDirectionX = 1;
+		}
+		else if (isLeftKey)
+		{
+			fDirectionX = -1;
+		}
+		else
+		{
+			fDirectionX = 0;
+		}
+		// 마우스를 누르는 동안
+        // Input.GetMouseButton()
+		// 마우스 버튼이 눌렸을 때
+        // Input.GetMouseButtonDown()
+		// 마우스 버튼을 놓았을 때
+        // Input.GetMouseButtonUp()
+		// () 안에 0(왼쪽), 1(오른쪽), 2(휠)
+		// 주석 한번에 넣기 : ctrl + k(누르고 떼기) + c
+		// 주석 한번에 풀기 : ctrl + k(누르고 떼기) + u
+		// 줄 정렬 : ctrl + k + f
+		// 전체 선택 : ctrl + a
+		//bool isLeftMouseDown = Input.GetMouseButtonDown(0);
+		//bool isLeftMouse = Input.GetMouseButton(0);
+
+		//if (isLeftMouseDown)
+		//{
+		//	Vector3 vecMousePosition = Input.mousePosition;
+		//	vecMousePosition.z += 10;
+		//	vecMousePosition.y -= 1;
+		//	Vector3 vecMouseWorldPosition = Camera.main.ScreenToWorldPoint(vecMousePosition);
+		//	Debug.Log(vecMouseWorldPosition.ToString());
+
+		//	transform.position = vecMouseWorldPosition;
+		//}
+		//else { }
+		//if (isLeftMouse)
+		//{
+		//	// transform.Rotate(0.3f, 0.2f, 0.1f);
+		//	Vector3 vecMousePosition = Input.mousePosition;
+		//	vecMousePosition.z += 10;
+		//	vecMousePosition.y -= 1;
+		//	Vector3 vecMouseWorldPosition = Camera.main.ScreenToWorldPoint(vecMousePosition);
+		//	Debug.Log(vecMouseWorldPosition.ToString());
+
+		//	transform.position = vecMouseWorldPosition;
+		//}
+		//else { }
+    }
+}
